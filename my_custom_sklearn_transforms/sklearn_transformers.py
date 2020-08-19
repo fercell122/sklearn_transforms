33b1b614311ec49da02f5ee435d412f2ba239f57
fercell122 <53842119+fercell122@users.noreply.github.com>
2020-08-19 16:45:47 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -16,15 +16,4 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return data.drop(labels=self.columns, axis='columns')
 
     
-   class transformisloga(BaseEstimator, TransformerMixin):
-    def __init__(self, colunas):
-        self.colunas = colunas
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        # Retornamos um novo dataframe com log
-        return data.log1p(labels=self.colunas, axis='colunas')
+ 
