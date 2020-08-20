0cdfc3d1eabf75c123ed46f2b495f9e7776ac922
fercell122 <53842119+fercell122@users.noreply.github.com>
2020-08-20 16:34:53 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -12,6 +12,14 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
+        data['H_AULA_PRES']= np.log1p(data['H_AULA_PRES'])                                                                          
+        data['TAREFAS_ONLINE']= np.log1p(data['TAREFAS_ONLINE'])                                      
+        data['FALTAS']= np.log1p(data['FALTAS'])
+        data['INGLES']= np.log1p(data['INGLES'])
+        data['NOTA_DE']= np.log1p(data['NOTA_DE'])
+        data['NOTA_EM']= np.log1p(data['NOTA_EM']) 
+        data['NOTA_MF']= np.log1p(data['NOTA_MF'])                                                                  
+        data['NOTA_GO']= np.log1p(data['NOTA_GO']) 
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
