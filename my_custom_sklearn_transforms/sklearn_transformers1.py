3c588639ba4a1d50bdaf6cccdc9a8058cec38133
fercell122 <53842119+fercell122@users.noreply.github.com>
2020-08-20 17:33:11 -0300

    Update sklearn_transformers1.py

--- my_custom_sklearn_transforms/sklearn_transformers1.py
+++ my_custom_sklearn_transforms/sklearn_transformers1.py
@@ -12,13 +12,13 @@ class transformisloga(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe com log
        #return   np.log1p(data[self.columns]) + data['PERFIL']???
-data['H_AULA_PRES']= np.log1p(data['H_AULA_PRES'])                                                                          
-data['TAREFAS_ONLINE']= np.log1p(data['TAREFAS_ONLINE'])                                      
-data['FALTAS']= np.log1p(data['FALTAS'])
-data['INGLES']= np.log1p(data['INGLES'])
-data['NOTA_DE']= np.log1p(data['NOTA_DE'])
-data['NOTA_EM']= np.log1p(data['NOTA_EM']) 
-data['NOTA_MF']= np.log1p(data['NOTA_MF'])                                                                  
-data['NOTA_GO']= np.log1p(data['NOTA_GO']) 
+        data['H_AULA_PRES']= np.log1p(data['H_AULA_PRES'])                                                                          
+        data['TAREFAS_ONLINE']= np.log1p(data['TAREFAS_ONLINE'])                                      
+        data['FALTAS']= np.log1p(data['FALTAS'])
+        data['INGLES']= np.log1p(data['INGLES'])
+        data['NOTA_DE']= np.log1p(data['NOTA_DE'])
+        data['NOTA_EM']= np.log1p(data['NOTA_EM']) 
+        data['NOTA_MF']= np.log1p(data['NOTA_MF'])                                                                  
+        data['NOTA_GO']= np.log1p(data['NOTA_GO']) 
 
-return data
+        return data
